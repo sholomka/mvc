6e682cfb3a5fd04d4d7922276f5a9d752c6e2f6b
sholomka <sholomka@gmail.com>
2017-04-23 22:50:35 +0300

    add

--- application/views/add_view.php
+++ application/views/add_view.php
@@ -33,15 +33,10 @@
                     </div>
 
                     <div class="modal-footer">
-                        <button class="btn btn-default" type="button" data-dismiss="modal">Предварительный просмотр</button>
                         <button class="btn btn-primary" type="submit">Сохранить изменения</button>
                     </div>
-
                 </form>
             </div>
-
-
-
         </div>
     </div>
 </div>
